03bfbe92d09244b4192aea8d341adc8e62545a71
Косолапов Алексей <alkos5@yandex.ru>
2026-05-21 20:15:27 +0300

    Initial commit: создан проект Blog Ideas

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// Находим элемент с id="root" в index.html и рендерим приложение
+ReactDOM.createRoot(document.getElementById('root')!).render(
+ <React.StrictMode> {/* StrictMode - помощник для поиска проблем */}
+ <App />
+ </React.StrictMode>,
+);
